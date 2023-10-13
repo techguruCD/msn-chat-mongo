4b853e588617abdbd439ef0649dd2fb608cf4d4c
techguruCD <colinkong0406@yahoo.com>
2023-10-13 07:22:07 -0700

    ?

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const passport = require('passport')
+const routes = require('./routes')
+const sequelize = require('./sequelize')
+
+const ioHandler = require('./ioHandler')
+
+const app = express();
+const httpServer = http.createServer(app)
+const io = require('socket.io')(httpServer, {
+    transports: ["websocket"]
+})
+
+const PORT = 3000;
+
+app.use(fileUpload())
+app.use(passport.initialize())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use('/', express.static(path.resolve(__dirname, 'client/build')));
+app.use('/upload', express.static(path.resolve(__dirname, 'upload')));
+app.use('/', routes)
+require('./passport')(passport)
+
+io.on('connection', ioHandler.onConnect)
+
+sequelize.authenticate().then(() => {
+    console.log('Database connection OK!');
+    httpServer.listen(PORT)
+}).catch(error => {
+    console.log('Unable to connect to the database: ', error)
+})
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express')
+const router = express.Router()
+const api = require('./api')
+
+router.use('/api', api)
+
+router.get('*', (req, res) => {
+    res.redirect('/')
+})
+
+module.exports = router;
